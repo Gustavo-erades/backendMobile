e4df738bf2d5472dd007b1f21e10ea03b30969f9
Gustavo-erades <eradesvilarinho@gmail.com>
2024-05-05 20:08:17 -0300

    ajustando tabela

--- tabela/index.php
+++ tabela/index.php
@@ -9,7 +9,26 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <link rel="shortcut icon" href="iconeDentista.png" type="image/x-icon">
     <title>Consultar Anestésicos Locais</title>
+    <style>
+        @media (max-width:767px){
+            thead{
+                display:none;
+            }
+            td{
+                display:grid;
+                gap:4px;
+                grid-template-columns: repeat(2,1fr);
+                text-align: center;
+            }
+            td::before{
+                content:attr(data-th) ": ";
+                font-weight: bold;
+                color:#0CABA8;
+            }
+        }
+    </style>
 </head>
 <body>
     <div class="container">
@@ -20,28 +39,28 @@
         </div>
         <div class="row mt-3">
             <table class="table table-striped table-hover">
-                <thead class="table-dark">
+                <thead>
                     <tr>
-                        <th>Anestésico</th>
-                        <th>Dose máxima (por kg peso corporal)</th>
-                        <th>Máximo absoluto (independente do peso)</th>
-                        <th>Número de tubetes</th>
+                        <th style="color:#0CABA8;">Anestésico</th>
+                        <th style="color:#0CABA8;">Dose máxima (por kg peso corporal)</th>
+                        <th  style="color:#0CABA8;">Máximo absoluto (independente do peso)</th>
+                        <th  style="color:#0CABA8;">Número de tubetes</th>
                     </tr>
                 </thead>
                 <tbody id="tableBody">
                     <?php
                         while($row=$result->fetch(PDO::FETCH_ASSOC)){
                             echo "<tr>";
-                            echo "<td>";
+                            echo "<td data-th='Anestésico'>";
                             echo $row['anestesicoLocal'];
                             echo "</td>";
-                            echo "<td>";
+                            echo "<td data-th='Dose por kg'>";
                             echo number_format($row['doseMaxima'], 1);
                             echo "</td>";
-                            echo "<td>";
+                            echo "<td data-th='Máx absoluto'>";
                             echo number_format($row['maximoAbsoluto'], 1);
                             echo "</td>";
-                            echo "<td>";
+                            echo "<td data-th='num tubetes'>";
                             echo number_format($row['numTubetes'], 1);
                             echo "</td>";
                             echo "</tr>";
